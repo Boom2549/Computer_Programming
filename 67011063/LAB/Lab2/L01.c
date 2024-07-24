50acc90e2f9e41f2b600d7e76ae5c07e7a8c616d
IOT-engineer <67011063@kmitl.ac.th>
2024-07-24 18:22:51 +0700

    Put all in

---
+++ 67011063/LAB/Lab2/L01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is %c\n", "A");
+    return 0;
+}
---
+++ 67011063/LAB/Lab3/L01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b, c, d, e;
+    a = 3+5*2*9/3-8;
+    b = 3.5+9-6*3*7/2;
+    c = 8.3*2+5*9/2+10*4;
+    d = 7/6+2-3*9*5*16;
+    e = 7.8+9-3.6*2/8+5*9-3.9;
+    printf("%f\n", a);
+    printf("%f\n", b);
+    printf("%f\n", 8.3*2+5*9/2+10*4);
+    printf("%f\n", d);
+    printf("%f\n", e);
+
+    return 0;
+}
